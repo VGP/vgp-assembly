1ccf3623ac054262486fb6c6a16637514bd7bdc0
Maria Simbirsky <msimbirsky@dnanexus.com>
2018-02-23 14:33:10 -0500

    Add 'details' field to symlinks containing AWS object key

--- AWS_bucket/vgp_upload_to_cloud.py
+++ AWS_bucket/vgp_upload_to_cloud.py
@@ -162,7 +162,8 @@ def main(path, profile, species_name, species_id, datatype, tissue=None):
                                 'symlinkPath': {
                                     "container": "{region}:{bucket}".format(region='us-east-1', bucket=VGP_BUCKET),
                                     "object": object
-                                    }
+                                    },
+                                'details': {'object-key': object}
                                 })
         print('Associated object {obj} with DNAnexus file {file}'.format(obj=object, file=file['id']))
 
