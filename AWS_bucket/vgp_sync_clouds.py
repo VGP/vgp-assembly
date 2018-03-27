09eb7f0f9e53a877e1eb7b41e2fb87280ef7715a
Maria Simbirsky <msimbirsky@dnanexus.com>
2018-03-27 17:36:35 -0400

    Automatically update drive credentials when script is called

--- AWS_bucket/vgp_sync_clouds.py
+++ AWS_bucket/vgp_sync_clouds.py
@@ -16,14 +16,21 @@ def locate_or_create_dx_drive(drive_name='genomeark'):
     # findDrives is an API method that has not been explicitly added to dxpy yet, so call the API method explicitly.
     drives = dxpy.DXHTTPRequest('/system/findDrives', {'name': drive_name}, always_retry=True)['results']
 
+    # use boto to read the profile info from aws config file
+    s3client = boto3.session.Session(profile_name=drive_name)
+    profile = s3client.get_credentials()
+    
     if len(drives) == 1:
-        return drives[0]
+        # Make sure the drive we found is up to date with the latest credentials
+        drive_id = drives[0]
+        update = {'credentials': {'accessKeyId': profile.access_key,
+                                  "secretAccessKey": profile.secret_key}}
+        drive_id = dxpy.DXHTTPRequest('/{0}/update'.format(drive_id), update)
+
+        return drive_id
     elif len(drives) == 0:
         # if no drive exists, create it
         print("Creating drive with name: {0}".format(drive_name))
-        # use boto to read the profile info from aws config file
-        s3client = boto3.session.Session(profile_name=drive_name)
-        profile = s3client.get_credentials()
 
         # create drive using API call
         new_drive_def = {'name': drive_name,
