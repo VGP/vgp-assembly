9a4b754402482a714b95153963165b5bc8aff225
arangrhie <arrhie@gmail.com>
2018-10-11 14:03:07 -0400

    Remove picard since we are not using it

--- pipeline/salsa/index.sh
+++ pipeline/salsa/index.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 module load bwa/0.7.17
-module load samtools/1.8
-module load picard/2.9.2
+module load samtools/1.9
 
 fa=$1
 
 echo "bwa index $fa"
 bwa index $fa
+echo
+
 echo "samtools faidx $fa"
 samtools faidx $fa
-echo "\
-java -jar -Xmx2g /usr/local/apps/picard/2.9.2/build/libs/picard.jar CreateSequenceDictionary R=$fa O=${fa/.fasta/.dict}"
-java -jar -Xmx2g /usr/local/apps/picard/2.9.2/build/libs/picard.jar CreateSequenceDictionary R=$fa O=${fa/.fasta/.dict}
+echo
+
