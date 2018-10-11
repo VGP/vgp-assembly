012eab487a0580f6bcb2397782de40e73bcab90a
Sergey Koren <sergek@umd.edu>
2018-10-11 17:24:16 -0400

    Update find_telomere.sh

--- pipeline/telomere/find_telomere.sh
+++ pipeline/telomere/find_telomere.sh
@@ -19,10 +19,10 @@ module load minimap2	# For sdust
 
 $VGP_PIPELINE/telomere/find_telomere $file > $prefix.telomere
 sdust $file > $prefix.sdust
-java $VGP_PIPELINE/telomere/SizeFasta $file > $prefix.lens
+java -cp $VGP_PIPELINE/telomere/telomere.jar SizeFasta $file > $prefix.lens
 
-java $VGP_PIPELINE/telomere/FindTelomereWindows $prefix.telomere 99.9 > $prefix.windows
-java $VGP_PIPELINE/telomere/FindTelomereBreaks $prefix.lens $prefix.sdust $prefix.telomere > $prefix.breaks
+java -cp $VGP_PIPELINE/telomere/telomere.jar FindTelomereWindows $prefix.telomere 99.9 > $prefix.windows
+java -cp $VGP_PIPELINE/telomere/telomere.jar FindTelomereBreaks $prefix.lens $prefix.sdust $prefix.telomere > $prefix.breaks
 
 cat $prefix.windows |awk '{if ($4 > 100000 && $3-$5 > 100000) print $0}'
 cat $prefix.breaks | awk '{if ($4 > 100000 && $NF-$6 > 100000) print $0}'
