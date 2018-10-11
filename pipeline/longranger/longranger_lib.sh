06de0e3b34e9cc256ae0255e69fd32d1c22c4732
arangrhie <arrhie@gmail.com>
2018-10-11 16:01:42 -0400

    for testing files under lib/

---
+++ pipeline/longranger/longranger_lib.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+	echo "Usage: ./longranger.sh <genome_id> <lib>"
+	echo "Assumes we have asm.fasta"
+	exit -1
+fi
+
+genome=$1
+lib=$2
+ref=asm.fasta
+ref=${ref/.fasta/}      ## if contains .fasta, remove it
+
+if ! [ -e "refdata-$ref/genome" ] ; then
+echo "No reference found"
+echo "=== start indexing reference ==="
+echo "\
+$tools/longranger/longranger-2.2.2/longranger mkref $ref.fasta"
+$tools/longranger/longranger-2.2.2/longranger mkref $ref.fasta
+echo ""
+fi
+
+echo "=== start running longranger wgs ==="
+$tools/longranger/longranger-2.2.2/longranger align \
+--id=${genome}_${lib} \
+--fastq=/data/rhiea/genome10k/$genome/genomic_data/10x/$lib \
+--sample=$genome \
+--reference=refdata-$ref \
+--jobmode=slurm \
+--localcores=32 \
+--localmem=60 \
+--maxjobs=500 \
+--jobinterval=100 \
+--disable-ui \
+--nopreflight
+
