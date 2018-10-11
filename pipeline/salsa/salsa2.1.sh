6d3ad67f45c0b1928267a0ad1f7a2ebffd47637c
arangrhie <arrhie@gmail.com>
2018-10-11 13:56:04 -0400

    Add scripts to run salsa2.1

---
+++ pipeline/salsa/salsa2.1.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+name=$1
+out=$2
+
+bam=$name.bam
+bed=$name.bed
+
+if [ -z $name ]; then
+	echo "Usage: ./salsa2.sh <name> <out>"
+	echo -e "\tSymlink re_sites.txt"
+	exit -1
+fi
+
+if ! [ -s $bed ]; then
+	module load bedtools
+	echo "bedtools bamtobed -i $bam > $bed"
+	bedtools bamtobed -i $bam > $bed
+fi
+
+source /data/Phillippy/tools/conda/etc/profile.d/conda.sh
+conda activate salsa_env
+
+fasta=$1.fasta
+
+enz=`cat re_bases.txt`
+
+mkdir -p $out
+
+echo "\
+python /data/Phillippy/tools/salsa2/SALSA-2.1/run_pipeline.py -a $fasta -l $fasta.fai -e $enz -b $bed -o $out -m yes -p yes"
+python /data/Phillippy/tools/salsa2/SALSA-2.1/run_pipeline.py -a $fasta -l $fasta.fai -e $enz -b $bed -o $out -m yes -p yes
+
