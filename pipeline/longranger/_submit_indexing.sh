a7e3ad36b15faba76099354696512966d965e632
arangrhie <arrhie@gmail.com>
2018-10-11 16:02:02 -0400

    Update to use VGP_PIPELINE

--- pipeline/longranger/_submit_indexing.sh
+++ pipeline/longranger/_submit_indexing.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 if [[ -z $1 ]] ; then
-	echo "Usage: ./_submit_longranger.sh <genome> <ref.fasta>"
+	echo "Usage: ./_submit_indexing.sh <genome> <ref.fasta>"
 	echo "<ref.fasta> will be linked to asm.fasta."
-	echo "Assumes we have 10x reads located under /data/rhiea/<genome>/genomic_data/10x/."
+	echo "This script is doing indexing only, in case it failed for large genomes."
 	exit -1
 fi
 
@@ -11,12 +11,12 @@ if ! [ -e asm.fasta ]; then
 	ln -s $2 asm.fasta
 fi
 
-cpus=2
-mem=12g
+cpus=8
+mem=32g
 name=$1.longrgr
-script=$VGP_PIPELINE/longranger/longranger.sh
+script=$VGP_PIPELINE/longranger/indexing.sh
 args=$1
-walltime=4-0
+walltime=2-0
 
 mkdir -p logs
 log=$PWD/logs/$name.%A_%a.log
