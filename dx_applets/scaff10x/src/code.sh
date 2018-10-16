e0503ed8171c55eae9dc90f493d0d42c71f33176
Maria Simbirsky <msimbirsky@dnanexus.com>
2018-10-16 13:49:03 -0400

    Add output prefix option to scaff10x

--- dx_applets/scaff10x/src/code.sh
+++ dx_applets/scaff10x/src/code.sh
@@ -129,12 +129,24 @@ main() {
 	gzip read-BC_2.fastq
 	read_bc1=$(dx upload read-BC_1.fastq.gz --brief)
 	read_bc2=$(dx upload read-BC_2.fastq.gz --brief)
-	dx-jobutil-add-output read_bc1 "$read_bc1" --class=file
-	dx-jobutil-add-output read_bc2 "$read_bc2" --class=file
+	if [[ $output_prefix != "" ]]; then
+	    read_bc1_name = "${output_prefix}".read-BC_1.fastq.gz
+	    read_bc2_name = "${output_prefix}".read-BC_2.fastq.gz
+    else
+        read_bc1_name = read-BC_1.fastq.gz
+        read_bc2_name = read-BC_2.fastq.gz
+    fi
+	dx-jobutil-add-output read_bc1 "$read_bc1" --class=file --name="$read_bc1_name"
+	dx-jobutil-add-output read_bc2 "$read_bc2" --class=file --name="$read_bc2_name"
     fi
     gzip scaffolds.fasta
     scaffold=$(dx upload scaffolds.fasta.gz --brief)
-    dx-jobutil-add-output scaffold "$scaffold" --class=file
+    if [[ $output_prefix != "" ]]; then
+        scaffold_name = "${output_prefix}".scaffolds.fasta.gz
+    else
+        scaffold_name = scaffolds.fasta.gz
+    fi
+    dx-jobutil-add-output scaffold "$scaffold" --class=file --name="$scaffold_name"
 
     for i in "${!other_outputs[@]}"; do
         dx-jobutil-add-output other_outputs "${other_outputs[$i]}" --class=array:file
