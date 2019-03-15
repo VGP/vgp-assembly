77e5ecac5d41c65173bd7ff669a3c8e7d4f50a6f
arangrhie <arrhie@gmail.com>
2019-03-15 15:01:01 -0400

    add a few varieties on running mashmap

---
+++ pipeline/mashmap/mashmap_pi90.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ref=$1.fasta
+qry=$2.fasta
+out=mashmap_${2}_to_${1}
+
+mkdir -p $out
+
+out=$out/out.map
+
+if [ ! -e $out ]; then
+	echo "\
+	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out --filter_mode one-to-one --pi 90"
+	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out --filter_mode one-to-one --pi 90
+	echo
+fi
+
+module load gnuplot
+
+cd mashmap_${2}_to_${1}
+
+echo "\
+$tools/mashmap/generateDotPlot png large out.map"
+$tools/mashmap/generateDotPlot png large out.map
+mv out.png ../mashmap_${2}_to_${1}.png
+
--- pipeline/mashmap/mashmap_small.sh
+++ pipeline/mashmap/mashmap_small.sh
@@ -6,21 +6,21 @@ out=mashmap_${2}_to_${1}_2k
 
 mkdir -p $out
 
-out=$out/out.map
+out=$out
 
-if [ ! -e $out ]; then
+if [ ! -e $out/out.map ]; then
 	echo "\
-	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out -s 2000 --filter_mode one-to-one"
-	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out -s 2000 --filter_mode one-to-one
+	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out/out.map -s 2000 --filter_mode one-to-one"
+	$tools/mashmap/mashmap -r $ref -q $qry -t $SLURM_CPUS_PER_TASK -o $out/out.map -s 2000 --filter_mode one-to-one
 	echo
 fi
 
 module load gnuplot
 
-cd mashmap_${2}_to_${1}
+cd $out
 
 echo "\
 $tools/mashmap/generateDotPlot png large out.map"
 $tools/mashmap/generateDotPlot png large out.map
-mv out.png ../mashmap_${2}_to_${1}.png
+mv out.png ../$out.png
 
