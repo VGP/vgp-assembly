f4e8f0b3908899b1f3499977629c47983bbd81fe
arangrhie <arrhie@gmail.com>
2019-03-19 11:42:06 -0400

    adding max cov limit to ignore repeats

--- pipeline/freebayes-polish/freebayes.sh
+++ pipeline/freebayes-polish/freebayes.sh
@@ -13,7 +13,7 @@ if [[ -z $sample ]]; then
     exit -1
 fi
 
-module load freebayes
+module load freebayes/1.2.0
 #### [+] Loading freebayes  1.2.0
 module load samtools
 #### [+] Loading samtools 1.8  ... 
@@ -33,6 +33,7 @@ if [ -e bcf/$i.done ]; then
     exit 0 ## Disable temporarily for testing
 fi
 
+# Initialize the bcf.$i.list, which tracks the list of bcf files 'done'.
 if [ -e bcf.$i.list ]; then
     rm bcf.$i.list
 fi
@@ -43,10 +44,13 @@ do
     contig=`sed -n ${j}p $fasta.fai | awk '{print $1}'`
     contig_no_pipe=`echo $contig | sed 's/|/_/g'`
     end=`sed -n ${j}p $fasta.fai | awk '{print $2}'`
-    if ! [ -e bcf/$contig_no_pipe.bcf ]; then
+    # Skip if bcf is not empty
+    if ! [ -s bcf/$contig_no_pipe.bcf ]; then
         echo "\
-        freebayes --bam $bam --region $contig:1-$end -f $fasta | bcftools view --no-version -Ou > bcf/$contig_no_pipe.bcf"
-        freebayes --bam $bam --region $contig:1-$end -f $fasta | bcftools view --no-version -Ou > bcf/$contig_no_pipe.bcf
+        freebayes --bam $bam --region $contig:1-$end --max-coverage 200 -f $fasta | bcftools view --no-version -Ou > bcf/$contig_no_pipe.bcf"
+        freebayes --bam $bam --region $contig:1-$end --max-coverage 200 -f $fasta | bcftools view --no-version -Ou > bcf/$contig_no_pipe.bcf
+    else
+	echo "Found bcf/$contig_no_pipe.bcf"
     fi
     echo "bcf/$contig_no_pipe.bcf" >> bcf.$i.list
 done
@@ -61,7 +65,7 @@ echo "## Clean up the intermediate bcf files" || exit -1
 
 for bcf_file in $(cat bcf.$i.list)
 do
-    if [ -z $bcf_file ]; then
+    if [ ! -s $bcf_file ]; then
 	echo "Skip rm.."
     else
 	echo "\
