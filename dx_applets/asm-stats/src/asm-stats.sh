09e09f37702acb45e6f1708d2c875c44fcc877be
Giulio Formenti <giulio.formenti@gmail.com>
2019-05-10 00:32:30 +0200

    .

--- dx_applets/asm-stats/src/asm-stats.sh
+++ dx_applets/asm-stats/src/asm-stats.sh
@@ -20,7 +20,7 @@ main() {
 
     echo "Value of asm: '$asm'"
     echo "Value of gsize: '$gsize'"
-    echo "Value of c: '$c'"
+    echo "Value of mode: '$mode'"
 
     # The following line(s) use the dx command-line tool to download your file
     # inputs to the local file system using variable names for the filenames. To
@@ -31,22 +31,75 @@ main() {
     gunzip asm.fasta.gz
 	
 	java -jar -Xmx1g /opt/java/fastaContigSize.jar asm.fasta
+    
+    printf "Scaffolds\n" > asm_stats.txt
+    
+    java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.fasta.len $gsize >> asm_stats.txt
+    
+    if [[ "$mode" == "c" ]]; then
 
-    fasta_len=$(dx upload asm.fasta.len --brief)
+    	asm_stats=$(dx upload asm_stats.txt --brief)
     
-    dx-jobutil-add-output fasta_len "$fasta_len" --class=file
-    
-    java -jar -Xmx1g /opt/java/lenCalcNGStats.jar $fasta_len $gsize
-    
-    
-    
-    dx-jobutil-add-output asm_stats "$asm_stats" --class=file
-    
-    if [[ "$c" == "c" ]]; then
+    	dx-jobutil-add-output asm_stats "$asm_stats" --class=file
+
 		exit 0
 	fi
 	
-	N_BASES=`awk '{sum+=$2; sumN+=$3} END {print (sum-sumN)}' $fasta.len`
+	N_BASES=`awk '{sum+=$2; sumN+=$3} END {print (sum-sumN)}' asm.fasta.len`
 	echo "N bases: $N_BASES"
+	
+	java -jar -Xmx2g /opt/java/fastaGetGaps.jar asm.fasta asm.gaps.txt
+	
+	awk -F "\t" '$4>3 {print $1"\t"$2"\t"$3}' asm.gaps.txt > asm.gaps.bed
+	
+	awk '{print $1"\t0\t"$(NF-1)}' asm.fasta.len > fasta.len.bed
+	
+	bedtools subtract -a fasta.len.bed -b asm.gaps.bed | awk '{print $1"\t"$NF-$(NF-1)}' > asm.contigs.len
+	
+	java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.contigs.len $gsize 1 > asm.contigs.stats
+
+    printf "\nContigs\n" >> asm_stats.txt	
+	cat asm.contigs.stats >> asm_stats.txt
+
+	java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.gaps.txt $gsize 3 > asm.gaps.stats
+
+    printf "\nGaps\n" >> asm_stats.txt	
+	cat asm.gaps.stats >> asm_stats.txt	
+
+    if [[ "$mode" != "p" ]]; then
+
+    	asm_stats=$(dx upload asm_stats.txt --brief)
+    
+    	dx-jobutil-add-output asm_stats "$asm_stats" --class=file
+
+		exit 0
+	fi
+
+	printf "\n=== Primary Stats ===\n" >> asm_stats.txt
+	grep "scaffold_" fasta.len > asm.p.len
+	java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.p.len $gsize > asm.p.stats
+
+	printf "\nScaffolds\n" >> asm_stats.txt
+	cat $asm.p.stats >> asm_stats.txt	
+
+	grep "scaffold_" asm.contigs.len > asm.contigs.p.len
+	java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.contigs.p.len $gsize 1 > asm.contigs.p.stats
+	printf "\nContigs\n" >> asm_stats.txt
+	cat $asm.contigs.p.stats >> asm_stats.txt
+
+	grep "scaffold_" asm.gaps > asm.gaps.p
+	java -jar -Xmx1g /opt/java/lenCalcNGStats.jar asm.gaps.p $gsize 3 > asm.gaps.p.stats
+	printf "\nGaps\n" >> asm_stats.txt
+	cat $asm.gaps.p.stats >> asm_stats.txt	
+
+	printf "\nExtract primary set\n" >> asm_stats.txt 
+	cut -f1 asm.p.len > asm.p.list
+	java -jar -Xmx1g /opt/java/fastaExtractFromList.jar asm.fasta asm.p.list asm.p.fasta
+
+	printf "\n=== Alt Stats ===\n" >> asm_stats.txt 
+	grep -v "scaffold_" fasta.len > asm.h.len
+	java -jar -Xmx1g $script/lenCalcNGStats.jar asm.h.len $gsize > asm.h.stats
+	cat $asm.h.stats >> asm_stats.txt	
+
 
 }
