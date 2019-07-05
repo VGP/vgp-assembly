f78ee4722ba4b0bb3736372ede80ca6a5a880b5b
Giulio Formenti <giulio.formenti@gmail.com>
2019-07-05 21:09:17 +0200

    QV calculator updated to have 3x 5x QVs

--- dx_applets/qv-calculator/src/qv-calculator.sh
+++ dx_applets/qv-calculator/src/qv-calculator.sh
@@ -29,19 +29,29 @@ main() {
 
 
 	echo "\
-	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp"
-	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp
-	NUM_BP=`cat $genome.numbp`
-	echo "Total bases > 3x: echo $NUM_BP" >> qv_report.txt
+	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp3"
+	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp3
+	NUM_BP3=`cat $genome.numbp3`
+	echo "Total bases > 3x: echo $NUM_BP3" >> qv_report.txt
+
+	awk '{if ($1=="genome" && $2>5) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp5"
+	awk '{if ($1=="genome" && $2>5) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp5
+	NUM_BP5=`cat $genome.numbp5`
+	echo "Total bases > 5x: echo $NUM_BP5" >> qv_report.txt
 
 	bcftools view -H ${var_name} | awk -F "\t" '{print $4"\t"$5}' | awk '{lenA=length($1); lenB=length($2); if (lenA < lenB ) {sum+=lenB-lenA} else if ( lenA > lenB ) { sum+=lenA-lenB } else {sum+=lenA}} END {print sum}' > $genome.numvar
 
 	if [ -e $genome.numvar ]; then
 		NUM_VAR=`cat $genome.numvar`
 		echo "Total num. bases subject to change: $NUM_VAR" >> qv_report.txt
-		QV=`echo "$NUM_VAR $NUM_BP" | awk '{print (-10*log($1/$2)/log(10))}'`
-		echo $QV > $genome.qv
-		echo "QV of this genome $genome: $QV" >> qv_report.txt
+		QV3=`echo "$NUM_VAR $NUM_BP3" | awk '{print (-10*log($1/$2)/log(10))}'`
+		echo $QV3 > $genome.qv3
+		echo "Approximate QV of this genome $genome: $QV3" >> qv_report.txt
+
+		QV5=`echo "$NUM_VAR $NUM_BP5" | awk '{print (-10*log($1/$2)/log(10))}'`
+		echo $QV5 > $genome.qv5
+		echo "QV of regiones above >5x for $genome: $QV5" >> qv_report.txt		
+		
 	fi
 
 
