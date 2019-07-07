8ff1e621bae2dce65ffb8f88ddf353de816bee5d
Giulio Formenti <giulio.formenti@gmail.com>
2019-07-07 18:02:04 -0400

    calculated on 5x and <avg cov*12

--- dx_applets/qv-calculator/src/qv-calculator.sh
+++ dx_applets/qv-calculator/src/qv-calculator.sh
@@ -10,13 +10,17 @@ main() {
 
     echo "Value of aligned_bam: '$BAM'"
     echo "Value of variant: '$VAR'"
+    echo "Value of csv: '$CSV'"
 
     bam_name=$BAM_name
     var_name=$VAR_name
+    csv_name=$CSV_name
 
     dx download "$BAM" -o ${bam_name}
 
     dx download "$VAR" -o ${var_name}
+    
+    dx download "$CSV" -o ${csv_name}
 
 	if [ -e aligned.genomecov ]; then
 		echo "aligned.genomecov already exists. skip..."
@@ -28,32 +32,26 @@ main() {
 	fi
 
 
+	mean_cov=`tail -n1 ${csv_name} | awk -F "," '{printf "%.0f\n", $17}'`	# parse out the mean_cov from summary.csv
+	h_filter=$((mean_cov*12))	# exclude any sites >12x
+	l_filter=5			# exclude any sites <5x
+	echo "Get numbp between $l_filter ~ $h_filter x"
+
 	echo "\
-	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp3"
-	awk '{if ($1=="genome" && $2>3) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp3
-	NUM_BP3=`cat $genome.numbp3`
-	echo "Total bases > 3x: echo $NUM_BP3" >> qv_report.txt
+	awk -v l=$l_filter -v h=$h_filter '{if (\$1=="genome" && \$2>l && \$2<h) {numbp += \$3}} END {print numbp}' aligned.genomecov > $genome.numbp"
+	awk -v l=$l_filter -v h=$h_filter '{if ($1=="genome" && $2>l && $2<h) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp
+	NUM_BP=`cat $genome.numbp`
+	echo "Total bases > 5x: $NUM_BP" >> qv_report.txt
 
-	awk '{if ($1=="genome" && $2>5) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp5"
-	awk '{if ($1=="genome" && $2>5) {numbp += $3}} END {print numbp}' aligned.genomecov > $genome.numbp5
-	NUM_BP5=`cat $genome.numbp5`
-	echo "Total bases > 5x: echo $NUM_BP5" >> qv_report.txt
-
-	bcftools view -H ${var_name} | awk -F "\t" '{print $4"\t"$5}' | awk '{lenA=length($1); lenB=length($2); if (lenA < lenB ) {sum+=lenB-lenA} else if ( lenA > lenB ) { sum+=lenA-lenB } else {sum+=lenA}} END {print sum}' > $genome.numvar
+	bcftools view -H -i 'QUAL>1 && (GT="AA" || GT="Aa") && INFO/DP>5' -Ov ${var_name} | awk -F "\t" '{print $4"\t"$5}' | awk '{lenA=length($1); lenB=length($2); if (lenA < lenB ) {sum+=lenB-lenA} else if ( lenA > lenB ) { sum+=lenA-lenB } else {sum+=lenA}} END {print sum}' > $genome.numvar
 
 	if [ -e $genome.numvar ]; then
 		NUM_VAR=`cat $genome.numvar`
-		echo "Total num. bases subject to change: $NUM_VAR" >> qv_report.txt
-		QV3=`echo "$NUM_VAR $NUM_BP3" | awk '{print (-10*log($1/$2)/log(10))}'`
-		echo $QV3 > $genome.qv3
-		echo "Approximate QV of this genome $genome: $QV3" >> qv_report.txt
-
-		QV5=`echo "$NUM_VAR $NUM_BP5" | awk '{print (-10*log($1/$2)/log(10))}'`
-		echo $QV5 > $genome.qv5
-		echo "QV of regiones above >5x for $genome: $QV5" >> qv_report.txt		
-		
+		echo "Total num. bases subject to change: $NUM_VAR"  >> qv_report.txt
+		QV=`echo "$NUM_VAR $NUM_BP" | awk '{print (-10*log($1/$2)/log(10))}'`
+		echo $QV > $genome.qv
+		echo "QV of this genome $genome: $QV"  >> qv_report.txt
 	fi
-
 
     qv_report=$(dx upload qv_report.txt --brief)
 
