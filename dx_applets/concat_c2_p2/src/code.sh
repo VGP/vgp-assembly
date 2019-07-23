82f81f9f15990518d41cfe03110db1ce22c364c0
Arkarachai <arkarachai.af@gmail.com>
2019-07-23 15:28:28 -0700

    add appet for c2 p2 and make workflow for purge_dups

---
+++ dx_applets/concat_c2_p2/src/code.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# concat_two_files 0.0.1
+
+
+main() {
+
+    echo "Value of input1: '$input1'"
+    echo "Value of input2: '$input2'"
+    echo "Value of output_extension: '$output_extension'"
+
+    dx download "$input1" -o input1
+    dx download "$input2" -o input2
+
+    cat input1 input2 > temp_output.fasta.gz
+
+    basename="$input1_prefix"
+    basename=${basename//.renamed}
+    basename=${basename%_c2}
+    basename=${basename%_p2}
+    basename="$basename"+"$output_extension"
+
+    mv temp_output.fasta.gz "$basename".fasta.gz
+
+    output=$(dx upload "$output_name" --brief)
+
+    dx-jobutil-add-output output "$output" --class=file
+}
---
+++ dx_workflows/scaffold_1_purge_dups/Readme.md
@@ -0,0 +1,5 @@
+# Update Logs
+
+2019-07-23
+
+* Initial commit VGP1.6
