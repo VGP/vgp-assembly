5d7334fa0077ee0f16d79acd67eab4f00d25b362
Arkarachai <arkarachai.af@gmail.com>
2019-08-03 18:42:14 -0700

    patch special version of freebayes

--- dx_scripts/sw_version.py
+++ dx_scripts/sw_version.py
@@ -5,21 +5,33 @@ import time
 
 app_id_hardcode_up_version={
     "app-FPkkQ4j0gjx97J1X1496B9zF":"purge_haplotig (bitbucket v1.0.3+ 1.Nov.2018)",
-    "applet-FZzYjYQ0j3b9pVP06qg4Q97Y": "github ca23030ccf4254dfd2d3a5ea90d0eed41c24f88b",
+    "applet-FZzYjYQ0j3b9pVP06qg4Q97Y": "purge_dups github ca23030ccf4254dfd2d3a5ea90d0eed41c24f88b",
     "app-FVVgJ7Q09zJpb0KZ9P3v1BpQ": "Solve3.2.1_04122018",
     "app-FVpb0j00px8fVZ9qPPGYxxP8": "Salsa 2.2",
     "app-FXF87GQ0yV32v3Q32v06xBvv": "smrtlink_6.0.0.47841",
     "app-Fb0JBK8012x8z3gG91Yxyj3q": "smrtlink_7.0.1.66975",
-    "applet-FZY5j400j3bP4b62GxKB057v": "freebayes 1.3.1",
     "app-FPgQ4Y8086pf03z5J04ZkXF3": "longranger 2.2.2",
+    "applet-FZY5j400j3bP4b62GxKB057v": "freebayes 1.3.1",
+    "applet-FYVY3X00x9Y0YpKZ6xgjQxZG": "freebayes v1.2.0-17-ga78ffc0 parallel by chr",
 }
+
 def latest_job(name_string):
-    job_id = subprocess.check_output('dx find jobs --name {0} --all-jobs --state done -n 1 --brief'.format(name_string),shell=True)
-    return job_id.strip()
+
+    if '||' not in name_string:
+        job_id = subprocess.check_output('dx find jobs --name {0} --all-jobs --state done -n 1 --brief'.format(name_string),shell=True)
+        return job_id.strip()
+    else:
+        name_list = name_string.split('||')
+        for name_list_member in name_list:
+            job_id = subprocess.check_output('dx find jobs --name {0} --all-jobs --state done -n 1 --brief'.format(name_list_member), shell=True)
+            if job_id != '':
+                return job_id.strip()
+
 
 def job_2_app(job_id):
     try:
         app_id = dxpy.describe(job_id)['app']
+
     except KeyError:
         app_id = dxpy.describe(job_id)['applet']
     return app_id.strip()
@@ -47,7 +59,7 @@ def start_time(job_id):
     return startedRunning
 
 falcon_job_id=latest_job("*alcon*aligner*")
-falcon_unzip_job_id=latest_job("*nzip*olish*")
+falcon_unzip_job_id=latest_job("*nzip*olish*||*Unzip*Haplotype*Arrow")
 purge_job_id=latest_job("*purge*")
 scaff10x_job_id=latest_job("Scaff10x*")
 bionano_job_id=latest_job("Bionano*")
@@ -56,18 +68,17 @@ polish_job_id=latest_job("polish")
 longranger_job_id=latest_job("10X*Longranger*Align*")
 freebayes_job_id=latest_job("Free*ayes*")
 
-attributes=[]
+
+
+print('\t'.join(['falcon','falcon_unzip','purge','scaff10x','bionano','salsa','polish','longranger','freebayes']))
 
 job_list = [falcon_job_id,falcon_unzip_job_id,purge_job_id,scaff10x_job_id,bionano_job_id,
               salsa_job_id,polish_job_id,longranger_job_id,freebayes_job_id]
+print('\t'.join(job_list))
+
 app_list = map(job_2_app,job_list)
-attributes.append(['falcon','falcon_unzip','purge','scaff10x','bionano','salsa','polish','longranger','freebayes'])
-attributes.append(job_list)
-attributes.append(map(start_time,job_list))
-attributes.append(app_list)
-attributes.append(map(app_2_version,app_list))
-attributes.append(map(app_2_upversion,app_list))
+print('\t'.join(map(start_time,job_list)))
+print('\t'.join(app_list))
+print('\t'.join(map(app_2_version,app_list)))
+print('\t'.join(map(app_2_upversion,app_list)))
 
-
-for attribute in attributes:
-    print('\t'.join(attribute))
