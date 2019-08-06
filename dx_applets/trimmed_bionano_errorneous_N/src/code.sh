89ecc8799b6de5992defb4227c6d678e985b79aa
Arkarachai <arkarachai.af@gmail.com>
2019-08-05 17:02:45 -0700

    change output name to use species code

--- dx_applets/trimmed_bionano_errorneous_N/src/code.sh
+++ dx_applets/trimmed_bionano_errorneous_N/src/code.sh
@@ -5,6 +5,7 @@
 main() {
 
     echo "Value of input_fastagz: '$input_fastagz'"
+    echo "Value of species_id: '$species_id'"
 
     echo "${input_fastagz_name: -3}"
     if [[ "${input_fastagz_name: -3}" == ".gz" ]]; then    
@@ -21,9 +22,10 @@ main() {
     python3 trim_Ns_DNAnexus.py input_trimmedN.fasta output_list.txt
     python3 clip_regions_DNAnexus.py input_trimmedN.fasta output_list.txt "${trimmed_name}.trimmed.fasta"
     gzip "${trimmed_name}.trimmed.fasta"
+    mv "${trimmed_name}.trimmed.fasta.gz" "${species_id}_s2.fasta.gz"
 
 
-    output_fastagz=$(dx upload "${trimmed_name}.trimmed.fasta.gz" --brief)
+    output_fastagz=$(dx upload "${species_id}_s2.fasta.gz" --brief)
     dx-jobutil-add-output output_fastagz "$output_fastagz" --class=file
 
     output_list=$(dx upload output_list.txt --brief)
