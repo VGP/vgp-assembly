7ab96caf5e3806e640fffa27250b09c9a2cf6f4e
arangrhie <arrhie@gmail.com>
2019-08-21 20:05:37 -0400

    add output bed

--- pipeline/mashmap/mashmap_pi90.sh
+++ pipeline/mashmap/mashmap_pi90.sh
@@ -24,3 +24,4 @@ $tools/mashmap/generateDotPlot png large out.map"
 $tools/mashmap/generateDotPlot png large out.map
 mv out.png ../mashmap_${2}_to_${1}.png
 
+cat out.map | awk -F " " -v name=$name '{print $6"\t"$8"\t"$9+1"\t"$1"\t"$3"\t"$4+1"\t"$1":"$3"-"$4+1":"name"\t"$NF"\t"$5"\t"$7"\t"$2}' > out.map.bed
