99435a17f4695935a22639b4562ae79a06f622c2
Giulio <gformenti@rockefeller.edu>
2019-09-23 19:18:06 -0400

    increased memory

--- pipeline/stats/asm_stats.sh
+++ pipeline/stats/asm_stats.sh
@@ -17,10 +17,10 @@ script=$VGP_PIPELINE/stats/
 asm=${fasta/.fasta/}
 
 if ! [ -e $fasta.len ]; then
-	java -jar -Xmx1g $script/fastaContigSize.jar $fasta
+	java -jar -Xmx2g $script/fastaContigSize.jar $fasta
 fi
 echo "Scaffolds"
-java -jar -Xmx1g $script/lenCalcNGStats.jar $fasta.len $gsize > $asm.stats
+java -jar -Xmx2g $script/lenCalcNGStats.jar $fasta.len $gsize > $asm.stats
 cat $asm.stats
 
 
@@ -43,13 +43,13 @@ fi
 
 echo "Contigs"
 #if [ ! -e $asm.contigs.stats ]; then
-	java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.contigs.len $gsize 1 > $asm.contigs.stats
+	java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.contigs.len $gsize 1 > $asm.contigs.stats
 #fi
 cat $asm.contigs.stats
 
 echo "Gaps"
 #if [ ! -e $asm.gaps.stats ]; then
-	java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.gaps $gsize 3 > $asm.gaps.stats
+	java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.gaps $gsize 3 > $asm.gaps.stats
 #fi
 cat $asm.gaps.stats
 
@@ -63,20 +63,20 @@ echo
 
 echo "=== Primary Stats ==="
 grep "scaffold_" $fasta.len > $asm.p.len
-java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.p.len $gsize > $asm.p.stats
+java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.p.len $gsize > $asm.p.stats
 
 echo "Scaffolds"
 cat $asm.p.stats
 echo
 
 grep "scaffold_" $asm.contigs.len > $asm.contigs.p.len
-java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.contigs.p.len $gsize 1 > $asm.contigs.p.stats
+java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.contigs.p.len $gsize 1 > $asm.contigs.p.stats
 echo "Contigs"
 cat $asm.contigs.p.stats
 echo
 
 grep "scaffold_" $asm.gaps > $asm.gaps.p
-java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.gaps.p $gsize 3 > $asm.gaps.p.stats
+java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.gaps.p $gsize 3 > $asm.gaps.p.stats
 echo "Gaps"
 cat $asm.gaps.p.stats
 echo
@@ -90,7 +90,7 @@ echo
 
 echo "=== Alt Stats ==="
 grep -v "scaffold_" $fasta.len > $asm.h.len
-java -jar -Xmx1g $script/lenCalcNGStats.jar $asm.h.len $gsize > $asm.h.stats
+java -jar -Xmx2g $script/lenCalcNGStats.jar $asm.h.len $gsize > $asm.h.stats
 cat $asm.h.stats
 echo
 
@@ -98,6 +98,6 @@ echo
 if [ ! -e $asm.h.fasta ]; then
     echo "Extract alt set"
     cut -f1 $asm.h.len > $asm.h.list
-    java -jar -Xmx1g $script/fastaExtractFromList.jar $asm.fasta $asm.h.list $asm.h.fasta
+    java -jar -Xmx2g $script/fastaExtractFromList.jar $asm.fasta $asm.h.list $asm.h.fasta
 fi
 #END
