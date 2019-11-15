9f7864cec4f092f545d6b907bc4b802ccac95b4b
Edgar Hernandez <edgarh@Edgars-MacBook-Pro-3.local>
2019-11-15 09:56:45 -0800

    added modules python/2.7 python/3.5 and perl/5.18.2

--- wdl_pipeline/docker_pipeline/bionano/_docker_bionano.sh
+++ wdl_pipeline/docker_pipeline/bionano/_docker_bionano.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-module load python/2.7
-module load perl/5.18.2
-module load R
+# Fix python/3.6 installation to prepend PATH 
+module load python/3.5
+python --version
+which python
 
+module load python/2.7
+python --version
+which python
+
+module load perl/5.18.2
+perl -v
+which perl
+
+module load R
+R --version
+which R
