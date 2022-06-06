4d64d8c8ca874450c39d527c89c936d0fe97d0a9
Nadolina <nadibraj@gmail.com>
2022-06-06 15:20:00 -0400

    adding BSD-3 clause to parse_mito_blast + creating dir for tools developed for galaxy
    parse_mito_blast.py configure differently for local and galaxy use - need two version on github, hence additional galaxy folder

--- pipeline/VGP_decontamination_pipe/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='parse_mito_blast',
-    version='1.0',
-    author='vgp-assembly',
-    author_email='vgl-bfx@mail.rockefeller.edu',
-    packages=find_packages()
-)
